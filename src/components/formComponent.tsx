f9c0713f1b3fff6325348b0a87ce433a20635e2b
ogdakke <daniel.wargh1@gmail.com>
2023-02-06 16:31:12 +0200

    logiikka toimii, pvm toimii, testit toimii

--- src/components/formComponent.tsx
+++ src/components/formComponent.tsx
@@ -1,125 +1,48 @@
-import { Inter, Noto_Sans_Wancho } from '@next/font/google'
+import React, { useState} from 'react'
+import { Inter} from '@next/font/google'
 import styles from "@/styles/Home.module.css"
 import {useForm, SubmitHandler} from 'react-hook-form'
-import React, { useState} from 'react'
 import * as Label from '@radix-ui/react-label'
-import { isFriday, parseISO } from 'date-fns'
+import { format, isFriday, parseISO } from 'date-fns'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// declare the types of inputs
+export interface Inputs {
+  cartValue: number
+  deliveryDistance: number
+  numberOfItems: number
+  date: Date
+  time: string
+}
+
 
 const FormComponent: React.FC = () => {
-  const [deliveryFee, setDeliveryFee] = useState(0)
-  // const [inputData, setInputData] = useState<Inputs>()
   
-  const date = new Date()
+const [deliveryFee, setDeliveryFee] = useState(0)
   
-  // declare the types of inputs
-  interface Inputs {
-    cartValue: number
-    deliveryDistance: number
-    numberOfItems: number
-    date: string
-    time: string
-  }
-
-/**
- * calculates the delivery fee from user-inputted data
- * @param {Inputs} data User inputted data from the form
- * @returns {number} fee - value of delivery fee
- */
-const calculateDeliveryFee = (data: Inputs): number => {
-  
-  // declare fee as a number
-  let fee: number = 0;
-  // get the inputs
-  const cartValue = data.cartValue
-  // Round delivery distance down - This is because while decimal inputs are possible, it makes no sense to handle them as decimals.
-  const deliveryDistance = Math.floor(data.deliveryDistance)
-  const numberOfItems = data.numberOfItems
-  // date from input
-  const date = parseISO(data.date)
-  // get the inputted hours from hh:mm by splitting before : 
-  const hours = data.time.split(":")[0]
-
-  console.log(cartValue, "from calculatefunc");
-  console.log(deliveryDistance, "from calculatefunc");
-  console.log(numberOfItems, "from calculatefunc");
-  console.log("is it friday: ",isFriday(date));
-  console.log(hours, "from timeInput", data.time);
-  
-  
-  // Calculate small order surcharge
-  if (cartValue < 10) {
-    let surcharge: number = 10 - cartValue
-    fee = +cartValue + surcharge
-    console.log(fee);   
-  }
-
-  // add the default delivery cost of 2€
-  fee += 2
-  // Calculate delivery distance fee
-  if (deliveryDistance > 1000) {
-    
-  } 
-
-  // Calculate item surcharge
-  if (numberOfItems >= 5) {
-    const itemSurcharge = (numberOfItems - 4) * 0.5
-    fee += itemSurcharge
-  }
-
-  // Apply bulk fee
-  if (numberOfItems > 12) {
-    fee += 1.2
-  }
-
-  // Apply Friday rush fee if time is between 15:00 and 19:00
-  const isFridayRush = parseInt(hours) >= 15 && parseInt(hours) < 19
-  if (isFridayRush) {
-    fee *= 1.2
-  }
-
-  // Ensure fee does not exceed 15€
-  fee = Math.min(fee, 15)
-
-  // Delivery is free if cart value is 100€ or more
-  if (cartValue >= 100) {
-    fee = 0
-  }
-
-  //  precision function to make it return in format xx.xx
-  const precise = (num: number) => {
-    return parseFloat(num.toPrecision(4))
-  }
-  
-  console.log(fee);
-  
-  setDeliveryFee(precise(fee))
-  return fee
-};
-
 // Here we get the current time and date for the ui on initial load. It gets overriden by the user inputting something else
+const date = new Date()
 // Date in yyyy-mm-dd format to match default "date" input format.
 const dateNow = date.toISOString().slice(0,10)
+
 // Time in hours and minutes. Since getHours & getMinutes return digits <= 9 as 0, 1, 2 ... and not 00, 01, 02, ... we need to pad with "0" to length 2.
 const hours = date.getHours().toString().padStart(2, '0')
 const minutes = date.getMinutes().toString().padStart(2, '0')
 const timeNow = `${hours}:${minutes}`
 
-
+// register to get get values from form, formstate to handle errors, and handleSubmit to handle submitting
 const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
+  // set the default value of time to be timeNow. 
   defaultValues: {
-    date: dateNow,
-    time: timeNow
+      time: timeNow
   },
-  mode: "onChange"
+  // mode: "onChange"
 })
 
 // On submit, call the calculateDeliveryFee function.
 const onSubmit: SubmitHandler<Inputs> = (data: Inputs) =>{
-  calculateDeliveryFee(data)
-
+  setDeliveryFee(calculateDeliveryFee(data))  
 } 
 return (
   <>
@@ -131,7 +54,7 @@ return (
       <input  className={inter.className}
       aria-label='cartValue'
       placeholder='eg. 10.50'
-      type="tel"
+      type="number"
       step={0.01}
 
       {...register("cartValue", 
@@ -175,9 +98,9 @@ return (
       Enter Date. Default is today
       </Label.Root>
       <input className={inter.className}
+      defaultValue={dateNow}
       aria-label='dateInput'
       type="date"    
-      // defaultValue={dateNow}
       {...register("date", {valueAsDate: true, required: true})} />
       {errors.date && <span>Input a valid date</span>}
     </div>
@@ -194,7 +117,7 @@ return (
       {errors.time && <span>Input a valid delivery time</span>}
     </div>
     
-    <button className={styles.inputButton} type="submit" value={"Calculate delivery costs"}>
+    <button aria-label='Calculate' className={styles.inputButton} type="submit">
       Calculate
     </button>
   </form> 
@@ -204,4 +127,107 @@ return (
   </>
   )
 }
-export default FormComponent
+
+/**
+ * calculates the delivery fee from user-inputted data
+ * @param {Inputs} data User inputted data from the form
+ * @returns {number} fee - value of delivery fee
+ */
+export const calculateDeliveryFee = (data: Inputs): number => {
+  
+  // declare fee as a number
+  let fee: number = 0;
+  // get the inputs
+  const cartValue = data.cartValue
+  if (cartValue < 0.01 || Number.isNaN(cartValue)) {
+    fee = 0
+  } 
+
+  // Round delivery distance down - This is because while decimal inputs are possible, it makes no sense to handle them as decimals.
+  let deliveryDistance = Math.floor(data.deliveryDistance)
+  /**
+   * checks if number is NaN or 0 or smaller
+   * @param deliveryDistance 
+   * @returns {number} deliveryDistance
+   */ 
+  const checkIfNegative = (deliveryDistance: number): number => {
+    if (deliveryDistance <= 0 || Number.isNaN(deliveryDistance)) {
+      return deliveryDistance = 1
+    }     
+    return deliveryDistance
+  }
+  checkIfNegative(deliveryDistance)
+  
+  const numberOfItems = data.numberOfItems
+  
+  // date from user input
+  const date = data.date
+
+  // get the user inputted hours from hh:mm by splitting before : 
+  const hours = data.time.split(":")[0]
+
+  // console.log(cartValue, "from calculatefunc");
+  // console.log(deliveryDistance, "from calculatefunc");
+  // console.log(numberOfItems, "from calculatefunc");
+  // console.log("is it friday: ",data.date ,isFriday(data.date));
+  // console.log(date, data.date);
+  // console.log(hours, "from timeInput", data.time);
+  
+  
+  // Calculate small order surcharge
+  if (cartValue < 10) {
+    let surcharge: number = 10 - cartValue
+    fee = +cartValue + surcharge
+  }
+
+  // add the default delivery cost of 2€
+  fee += 2
+  // Calculate delivery distance fee
+  if (deliveryDistance > 1000) {
+    const additionalDistance = (deliveryDistance - 1000)
+    
+    let quotient = ~~(additionalDistance / 500)
+    fee += quotient
+    
+    if ((additionalDistance/500 != 0) && additionalDistance % 500 > 0 && additionalDistance % 500 < 500) {
+      fee += 1
+    }
+  } 
+
+
+  // Calculate item surcharge
+  if (numberOfItems >= 5) {
+    const itemSurcharge = (numberOfItems - 4) * 0.5
+    fee += itemSurcharge
+  }
+
+  // Apply bulk fee
+  if (numberOfItems > 12) {
+    fee += 1.2
+  }
+
+  // Apply Friday rush fee if time is between 15:00 and 19:00
+  const isFridayRush = parseInt(hours) >= 15 && parseInt(hours) < 19
+  if (isFridayRush && isFriday(date)) {
+    fee *= 1.2
+  }
+
+  // Ensure fee does not exceed 15€
+  fee = Math.min(fee, 15)
+
+  // Delivery is free if cart value is 100€ or more
+  if (cartValue >= 100) {
+    fee = 0
+  }
+
+  //  precision function to make it return in format xx.xx
+  const precise = (num: number) => {
+    return parseFloat(num.toPrecision(4))
+  }
+  
+  
+  return precise(fee)
+}
+
+
+export default FormComponent 
--- tests/components/formComponent.spec.tsx
+++ tests/components/formComponent.spec.tsx
@@ -3,22 +3,16 @@ import "@testing-library/jest-dom"
 import {screen, render, fireEvent, } from '@testing-library/react'
 import user from "@testing-library/user-event"
 
-import FormComponent from "../../src/components/formComponent";
+import FormComponent, { calculateDeliveryFee } from "../../src/components/formComponent";
 
 describe('formComponent', () => {
-  const data = {
-    cartValue: "1",
-    date: "2024-03-22",
-    deliveryDistance: "3",
-    numberOfItems: "4",
-    time: "18:30"
-  }
+  
 
 
   
 test('should render form with all the fields.', () => {
   render(<FormComponent />)
-  const submitButton = screen.getByDisplayValue('Calculate delivery costs')
+  const submitButton = screen.getByLabelText('Calculate')
 
   expect(
     screen.getByLabelText("cartValue")
@@ -41,31 +35,65 @@ test('should render form with all the fields.', () => {
   
 })
 
-test('Should submit correct form data', async () => {
-  const CARTVALUE = "10"
-  const DELIVERYDISTANCE = "1499"
-  const NUMBEROFITEMS = "4"
-  const DATEINPUT = "2023-03-22"
-  const TIMEINPUT = "18:30"
-  const FEE = 3.6 
+test('Should Calculate correct value for deliveryFee', () => {
+  // test for friday rush fee and surcharge fee
+  const data = { cartValue: 1,
+    date: new Date(2023, 1, 3),
+    deliveryDistance: 3,
+    numberOfItems: 4,
+    time: "18:30"}
+  expect(calculateDeliveryFee(data)).toBe(14.4)
+
+  // test for no additional fees other than default deliveryFee
+  const data2 = {cartValue: 10, deliveryDistance: 999, numberOfItems: 4, date: new Date(2023, 2, 5), time: "08.00"}
+  expect(calculateDeliveryFee(data2)).toBe(2)
+
+  // test for over 100€ cart value
   
-  render(<FormComponent/>)
+  // test for delivery fee never going above 15
 
-  const cartInput = screen.getByLabelText("cartValue")
-    cartInput.textContent = CARTVALUE
-  const deliveryInput = screen.getByLabelText("deliveryDistance")
-    deliveryInput.textContent = DELIVERYDISTANCE
-  const itemsInput = screen.getByLabelText("numberOfItems")
-    itemsInput.textContent = NUMBEROFITEMS
-  const dateInput = screen.getByLabelText("dateInput")
-    user.type(dateInput, DATEINPUT)
-  const timeInput = screen.getByLabelText("timeInput")
-    user.type(timeInput, TIMEINPUT)
-
-  const submitButton = screen.getByDisplayValue('Calculate delivery costs')
-
-  fireEvent.click(submitButton)
-  expect(await screen.findByLabelText("deliveryFee")).toHaveTextContent(`Delivery fee: ${FEE} €`)
-})
+  // test for edge cases in delivery distance. for ex. 1000, 1001, 1501, 2499 etc...
+  // also negative numbers
+  const distances = [1000, 1001, 1499, 1500, 1501, 2999, 4006, 8472, 0, -19, 0.000001]
+  // map over the destances to 
+  const values = distances.map(distance => calculateDeliveryFee({
+    cartValue: 10,
+    date: new Date(2023, 2, 4),
+    deliveryDistance: distance,
+    numberOfItems: 4,
+    time: "18:30"
+  }))
+  
+  expect(values).toStrictEqual([2,3,3,3,4,6,9,15,2,2,2])
+  })
 
 })
+
+
+// test('Should submit correct form data', async () => {
+//   const CARTVALUE = "10"
+//   const DELIVERYDISTANCE = "1499"
+//   const NUMBEROFITEMS = "4"
+//   const DATEINPUT = "2023-03-22"
+//   const TIMEINPUT = "18:30"
+//   const FEE = 3.6 
+  
+//   render(<FormComponent/>)
+
+//   const cartInput = screen.getByLabelText("cartValue")
+//     cartInput.textContent = CARTVALUE
+//   const deliveryInput = screen.getByLabelText("deliveryDistance")
+//     deliveryInput.textContent = DELIVERYDISTANCE
+//   const itemsInput = screen.getByLabelText("numberOfItems")
+//     itemsInput.textContent = NUMBEROFITEMS
+//   const dateInput = screen.getByLabelText("dateInput")
+//     user.type(dateInput, DATEINPUT)
+//   const timeInput = screen.getByLabelText("timeInput")
+//     user.type(timeInput, TIMEINPUT)
+
+//   const submitButton = screen.getByLabelText('Calculate')
+
+//   fireEvent.click(submitButton)
+//   expect(await screen.findByLabelText("deliveryFee")).toHaveTextContent(`Delivery fee: ${FEE} €`)
+// })
+
